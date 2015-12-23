abd2922ad2d7f8520cd98f812d4cb2988e03ef38
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-12-23 10:13:59 +0300

    some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
   // Config for our build files
   output: {
-    path: root('__build__'),
+    path: root('src/public/build'),
     filename: '[name].js',
     sourceMapFilename: '[name].map',
     chunkFilename: '[id].chunk.js'
@@ -31,11 +31,14 @@ module.exports = {
 
   resolve: {
     // ensure loader extensions match
-    extensions: ['','.ts','.js','.json', '.css', '.html']
+    extensions: ['', '.ts', '.js', '.json', '.css', '.html']
   },
 
   module: {
-    preLoaders: [ { test: /\.ts$/, loader: 'tslint-loader' } ],
+    preLoaders: [{
+      test: /\.ts$/,
+      loader: 'tslint-loader'
+    }],
     loaders: [
       // Support for .ts files.
       {
@@ -46,28 +49,46 @@ module.exports = {
             2403, // 2403 -> Subsequent variable declarations
             2300, // 2300 -> Duplicate identifier
             2374, // 2374 -> Duplicate number index signature
-            2375  // 2375 -> Duplicate string index signature
+            2375 // 2375 -> Duplicate string index signature
           ]
         },
-        exclude: [ /\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/ ]
+        exclude: [/\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/]
       },
 
       // Support for *.json files.
-      { test: /\.json$/,  loader: 'json-loader' },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
 
       // Support for CSS as raw text
-      { test: /\.css$/,   loader: 'raw-loader' },
+      {
+        test: /\.css$/,
+        loader: 'raw-loader'
+      },
 
       // support for .html as raw text
-      { test: /\.html$/,  loader: 'raw-loader' },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader'
+      },
     ],
-    noParse: [ /.+zone\.js\/dist\/.+/, /.+angular2\/bundles\/.+/ ]
+    noParse: [/.+zone\.js\/dist\/.+/, /.+angular2\/bundles\/.+/]
   },
 
   plugins: [
-    new CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js', minChunks: Infinity }),
-    new CommonsChunkPlugin({ name: 'common', filename: 'common.js', minChunks: 2, chunks: ['app', 'vendor'] })
-   // include uglify in production
+    new CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.js',
+      minChunks: Infinity
+    }),
+    new CommonsChunkPlugin({
+      name: 'common',
+      filename: 'common.js',
+      minChunks: 2,
+      chunks: ['app', 'vendor']
+    })
+    // include uglify in production
   ],
 
   // Other module loader config
@@ -78,8 +99,7 @@ module.exports = {
   // our Webpack Development Server config
   devServer: {
     historyApiFallback: true,
-    contentBase: 'src/public',
-    publicPath: '/__build__'
+    contentBase: 'src/public'
   }
 };
 
